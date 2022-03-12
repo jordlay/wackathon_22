4cebcaf718c807628fb50a62390f4533c9ff07f9
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-03-12 18:35:01 +0000

    button functions

--- Assets/Scripts/buttons.cs
+++ Assets/Scripts/buttons.cs
@@ -8,23 +8,28 @@ public class buttons : MonoBehaviour
     public void HomeButtonClicked()
     {
         SceneManager.LoadScene("Homepage");
+        Debug.Log("HOME");
     }
 
     public void CalendarButtonClicked()
     {
         SceneManager.LoadScene("Calendar");
+        Debug.Log("CALENDAR");
     }
 
     public void NewsfeedButtonClicked()
     {
         SceneManager.LoadScene("Newsfeed");
+        Debug.Log("NEWSFEED");
     }
     public void RecycleSearchButtonClicked()
     {
         SceneManager.LoadScene("RecycleSearch");
+        Debug.Log("RECYABLE");
     }
     public void HelpButtonClicked()
     {
         SceneManager.LoadScene("Help");
+        Debug.Log("HELP");
     }
 }
