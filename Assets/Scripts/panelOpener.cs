026544344a1b7dc6a0369cae34615453e60d68fa
Aimee Dowell <60100829+aimeedowell@users.noreply.github.com>
2022-03-13 11:28:54 +0000

    oufoasi

---
+++ Assets/Scripts/panelOpener.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelOpener : MonoBehaviour
+{
+    public GameObject Panel;
+    public void OpenPanel() {
+       StartCoroutine(TimerCoroutine());
+    }
+
+    void Start() {
+
+        Panel.SetActive(false);
+        
+    }
+
+
+    IEnumerator TimerCoroutine() {
+         if (Panel != null) {
+            // bool isActive = Panel.activeSelf;
+            Panel.SetActive(true);
+            yield return new WaitForSeconds(3.0f);
+            // StartCoroutine(TimerCoroutine());
+            Panel.SetActive(false);
+        }
+        // yield return new WaitForSeconds(5);
+    }
+}
